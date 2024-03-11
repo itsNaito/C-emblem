7809ae788377a621014f7ffdb15985c542c662ac
itsNaito <ifanfair04@gmail.com>
2024-03-11 17:49:48 -0400

    Stats enum for the stats in the game that characters can have

--- src/model/Stats.hpp
+++ src/model/Stats.hpp
@@ -1,1 +1,24 @@
 #pragma once
+
+/**
+ * enum is responsible for the name of each of the 8 stats in the game
+ * Each stat is responsible for a different characteristic
+ * Strength effect physical attacks
+ * Magic effects magic attacks
+ * Dexterity effects hit rate and crit rate
+ * Speed effects avoidance and number of hits during an attack
+ * Defense reduce damage taken from physical attacks
+ * Hp is for character health
+ * resistance reduces the damage from magic attacks
+ * luck reduces the chance to be crit on
+*/
+enum Stats{
+    STRENGTH,
+    MAGIC,
+    DEXTERITY,
+    SPEED,
+    DEFENSE,
+    HP,
+    RESISTANCE,
+    LUCK
+};
