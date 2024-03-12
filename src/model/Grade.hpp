540b72a2ce9080dd91311282aac7e7d0f47d7464
itsNaito <ifanfair04@gmail.com>
2024-03-12 17:14:11 -0400

    created an enum for grading weapons,armor, and classes

---
+++ src/model/Grade.hpp
@@ -0,0 +1,12 @@
+/**
+ * The grade enum is going to be responsible for grading weapons, classes, and armor
+ * The grade will determine if that classes is a high enough tier to use the weapon
+ * For example: An A class character can use any weapon from grade A or lower
+*/
+enum{
+    S=4,
+    A=3,
+    B=2,
+    C=1,
+    D=0
+};
